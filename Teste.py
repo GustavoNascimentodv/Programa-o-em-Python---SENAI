4225fc5e90d1d0f74f6d1733e10eb50b0e7814f0
Gustavo Nascimento <gustavo147batista@gmail.com>
2025-02-27 21:43:45 -0300

    Add files via upload

---
+++ Teste.py
@@ -0,0 +1,24 @@
+print(' Soma ')
+soma = int(input(' Digite um número: '))
+soma2 = int(input(' Digite outro número: '))
+print(' O resultado da soma é: ',( soma + soma2))
+
+
+print(' Subtração ')
+subtração = int(input(' Digite um número: '))
+subtração2 = int(input(' Digite outro número: '))
+print(' O resultado é: ',( subtração - subtração2))
+
+
+print(' Divisão ')
+divisão = float(input(' Digite um número: '))
+divisão2 = float(input(' Digite outro número: '))
+print(' O resultado é: ',( divisão / divisão2))
+
+
+print(' Multiplicação ')
+multiplicação = int(input(' Digite um número: '))
+multiplicação2 = int(input(' Digite outro número: '))
+print(' O resultado é: ',( multiplicação * multiplicação2))
+
+
